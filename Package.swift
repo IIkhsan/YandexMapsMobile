dbb573cd23f01310cf5dcd7c3d9824d512d53afe
Ilyas Ikhsanov <ilyas.ikhsanov@x5.ru>
2024-06-22 15:12:33 +0300

    Обновил яндекс карты с правкой для стора

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YandexMapsMobile",
-            url: "https://github.com/c-villain/YandexMapsMobile/releases/download/4.6.1/YandexMapsMobile.xcframework.zip",
-            checksum: "c3f659bd0389d487322616a1aa0515b1b84a335941fe55fa073882a7a4705a58"
+            url: "https://github.com/IIkhsan/YandexMapsMobile/releases/download/4.6.1/YandexMapsMobile.xcframework.zip",
+            checksum: "f56098b6d067205e68340b3d9b2d38eb6964d9e3c2472c470f600ffb6d3e4e0d"
         ),
     ]
 )
